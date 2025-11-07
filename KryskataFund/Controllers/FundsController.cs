25afdaaa8dbab09ed33b68226e52cd632bdfe5c4
Christian School <chrisangelov7@gmail.com>
2025-11-07 12:35:14 +0200

    Creted the fund controller

---
+++ KryskataFund/Controllers/FundsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KryskataFUnd.Controllers
+{
+    public class FundsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
