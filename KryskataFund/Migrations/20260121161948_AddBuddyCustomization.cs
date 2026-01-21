bbfdbce1eff4bf151a23233ad5134153996a6c5b
Kryskata <christianangelovv@gmail.com>
2026-01-21 18:25:12 +0200

    Added migration for buddy customization

---
+++ KryskataFund/Migrations/20260121161948_AddBuddyCustomization.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KryskataFund.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBuddyCustomization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuddyGlasses",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuddyHat",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuddyMask",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuddyGlasses",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BuddyHat",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BuddyMask",
+                table: "Users");
+        }
+    }
+}
