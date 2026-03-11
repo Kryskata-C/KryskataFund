a1a0d0f434bb9f340ed61e8f8a72fb4c1508ff34
Kryskata-C <christianangelovv@gmail.com>
2026-03-11 22:15:49 +0200

    Add RecurringDonation model

---
+++ KryskataFund/Models/RecurringDonation.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KryskataFund.Models
+{
+    public class RecurringDonation
+    {
+        public int Id { get; set; }
+
+        public int FundId { get; set; }
+
+        public int UserId { get; set; }
+
+        [Required]
+        public string DonorName { get; set; } = "Anonymous";
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime? CancelledAt { get; set; }
+
+        // Navigation property
+        public Fund? Fund { get; set; }
+    }
+}
