ca3b7a55a48b25a8ec1d9f9f26bea38425455aca
Kryskata-C <christianangelovv@gmail.com>
2026-03-14 20:05:24 +0200

    Extend ErrorViewModel with StatusCode, ErrorMessage, ErrorTitle

--- KryskataFund/Models/ErrorViewModel.cs
+++ KryskataFund/Models/ErrorViewModel.cs
@@ -3,6 +3,9 @@ namespace KryskataFund.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        public int StatusCode { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? ErrorTitle { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
