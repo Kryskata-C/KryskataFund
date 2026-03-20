e95ce858b4fe4f18d48895f654bae0d6173294fa
kryskata <christianangelovv@gmail.com>
2026-03-20 21:55:13 +0200

    Add FundComment model

---
+++ KryskataFund/Models/FundComment.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KryskataFund.Models
+{
+    public class FundComment
+    {
+        public int Id { get; set; }
+
+        public int FundId { get; set; }
+
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(1000)]
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
