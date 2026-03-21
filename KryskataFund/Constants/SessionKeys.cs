a7ca3058f8e5e772be75e12df2c17ba91bec15ad
kryskata <christianangelovv@gmail.com>
2026-03-21 15:05:24 +0200

    Add SessionKeys constants class to eliminate magic strings

---
+++ KryskataFund/Constants/SessionKeys.cs
@@ -0,0 +1,10 @@
+namespace KryskataFund.Constants
+{
+    public static class SessionKeys
+    {
+        public const string IsSignedIn = "IsSignedIn";
+        public const string UserId = "UserId";
+        public const string UserEmail = "UserEmail";
+        public const string IsAdmin = "IsAdmin";
+    }
+}
